3dd89eac2e77d6a65c5a9fc6cd6942359fc7cf08
Kyle Harman <kyle@Kyles-MacBook-Pro.local>
2020-04-02 16:28:20 +1100

    Gitignore issue fixed

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    // prettier-ignore
-    mongoURI:"mongodb+srv://kyle-harman:tNb4U7esuu7sKE7H@cluster0-9ukxr.mongodb.net/mern-auth?retryWrites=true&w=majority",
-    secretOrKey: "secret"
-};
