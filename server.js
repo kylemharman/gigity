98d57f07f2a32a02744fa60ec1844f24b73446f0
Kyle Harman <kyle@Kyles-MacBook-Pro.local>
2020-04-02 16:53:06 +1100

    commit before heroku deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 
@@ -33,6 +34,15 @@ require("./config/passport")(passport);
 
 // Routes
 app.use("/api/users", users);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
